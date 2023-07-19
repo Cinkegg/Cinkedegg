428d1b8f56cf16554463059cac6f0561c579f263
anyxia <fedotovaanastasia287@gmail.com>
2023-07-19 14:25:47 +0200

    zmeny

---
+++ sandbox/html+css/script.js
@@ -0,0 +1,20 @@
+console.log("Hello world");
+let pokusy = 0;
+console.log(pokusy);
+
+function myFunction(p1, p2) {
+    return p1 * p2; // The function returns the product of p
+}
+let vysledek = myFunction(4, 3);
+console.log(vysledek);
+
+
+
+function ahoj(parametr) {
+    alert("Ahoj" + parametr);
+}
+
+let poleCisel = [1, 2, 3, 4, 5]
+let poelTextu = ["Hello", "Hi"]
+console.log(poleCisel[0]); // = 1
+
